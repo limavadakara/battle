caed243893e47e66c24610fcd7d2a811f5bd3579
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-21 16:07:03 +0000

    Player 1 attacks player 2 and gets a confirmation for it

--- app.rb
+++ app.rb
@@ -18,7 +18,18 @@ enable :sessions
    @player_1 = session[:player_1]
    @player_2 = session[:player_2]
    @player_2_hit_point = 10
+   @attack_confirmation = ""
    erb(:play)
+
+ end
+
+ post "/attack"  do
+   @player_1 = session[:player_1]
+   @player_2 = session[:player_2]
+   @player_2_hit_point = 10
+   @attack_confirmation = "Player 2 attacked successfully!"
+   erb(:play)
+   
  end
  run! if app_file == $0
 end
---
+++ spec/features/attack_player_2_spec.rb
@@ -0,0 +1,7 @@
+feature 'Player 1 can attack player 2' do
+  scenario 'Player 1 attacks player 2 and gets a confirmation for the attack' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content("Player 2 attacked successfully!")
+  end
+end
